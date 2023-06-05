dd44a10993314da68400983556323305d90fafaf
Farida Arum Parwati <faridaarum1727@gmail.com>
2023-06-05 19:41:53 +0700

    first commit

---
+++ lib/components/login_form.dart
@@ -0,0 +1,125 @@
+import 'dart:convert';
+
+import 'package:doctor_appointment_app/components/button.dart';
+import 'package:doctor_appointment_app/main.dart';
+import 'package:doctor_appointment_app/models/auth_model.dart';
+import 'package:doctor_appointment_app/providers/dio_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../utils/config.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({Key? key}) : super(key: key);
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController(text: "jaki@gmail.com");
+  final _passController = TextEditingController(text: "11223344");
+  bool obsecurePass = true;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            cursorColor: Color.fromARGB(255, 24, 79, 199),
+            decoration: const InputDecoration(
+              hintText: 'Email Address',
+              labelText: 'Email',
+              alignLabelWithHint: true,
+              prefixIcon: Icon(Icons.email_outlined),
+              prefixIconColor: Color.fromARGB(255, 24, 79, 199),
+            ),
+          ),
+          Config.spaceSmall,
+          TextFormField(
+            controller: _passController,
+            keyboardType: TextInputType.visiblePassword,
+            cursorColor: Color.fromARGB(255, 24, 79, 199),
+            obscureText: obsecurePass,
+            decoration: InputDecoration(
+                hintText: 'Password',
+                labelText: 'Password',
+                alignLabelWithHint: true,
+                prefixIcon: const Icon(Icons.lock_outline),
+                prefixIconColor: Color.fromARGB(255, 24, 79, 199),
+                suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        obsecurePass = !obsecurePass;
+                      });
+                    },
+                    icon: obsecurePass
+                        ? const Icon(
+                            Icons.visibility_off_outlined,
+                            color: Colors.black38,
+                          )
+                        : const Icon(
+                            Icons.visibility_outlined,
+                            color: Color.fromARGB(255, 24, 79, 199),
+                          ))),
+          ),
+          Config.spaceSmall,
+          Consumer<AuthModel>(
+            builder: (context, auth, child) {
+              return Button(
+                width: double.infinity,
+                title: 'Sign In',
+                onPressed: () async {
+                  //login here
+                  final token = await DioProvider()
+                      .getToken(_emailController.text, _passController.text);
+
+                  if (token) {
+                    //auth.loginSuccess(); //update login status
+                    //rediret to main page
+
+                    //grab user data here
+                    final SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+                    final tokenValue = prefs.getString('token') ?? '';
+
+                    if (tokenValue.isNotEmpty && tokenValue != '') {
+                      //get user data
+                      final response = await DioProvider().getUser(tokenValue);
+                      if (response != null) {
+                        setState(() {
+                          //json decode
+                          Map<String, dynamic> appointment = {};
+                          final user = json.decode(response);
+
+                          //check if any appointment today
+                          for (var doctorData in user['doctor']) {
+                            //if there is appointment return for today
+
+                            if (doctorData['appointments'] != null) {
+                              appointment = doctorData;
+                            }
+                          }
+
+                          auth.loginSuccess(user, appointment);
+                          MyApp.navigatorKey.currentState!.pushNamed('main');
+                        });
+                      }
+                    }
+                  }
+                },
+                disable: false,
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:doctor_appointment_app/main_layout.dart';
+import 'package:doctor_appointment_app/models/auth_model.dart';
+import 'package:doctor_appointment_app/screens/auth_page.dart';
+import 'package:doctor_appointment_app/screens/booking_page.dart';
+import 'package:doctor_appointment_app/screens/success_booked.dart';
+import 'package:doctor_appointment_app/utils/config.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  //this is for push navigator
+  static final navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  Widget build(BuildContext context) {
+    //define ThemeData here
+    return ChangeNotifierProvider<AuthModel>(
+      create: (context) => AuthModel(),
+      child: MaterialApp(
+        navigatorKey: navigatorKey,
+        title: 'Flutter Doctor App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          //pre-define input decoration
+          inputDecorationTheme: const InputDecorationTheme(
+            focusColor: Color.fromARGB(255, 24, 79, 199),
+            border: Config.outlinedBorder,
+            focusedBorder: Config.focusBorder,
+            errorBorder: Config.errorBorder,
+            enabledBorder: Config.outlinedBorder,
+            floatingLabelStyle: TextStyle(color: Color.fromARGB(255, 24, 79, 199)),
+            prefixIconColor: Colors.black38,
+          ),
+          scaffoldBackgroundColor: Colors.white,
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            backgroundColor: Color.fromARGB(255, 24, 79, 199),
+            selectedItemColor: Colors.white,
+            showSelectedLabels: true,
+            showUnselectedLabels: false,
+            unselectedItemColor: Color.fromARGB(255, 204, 189, 189),
+            elevation: 10,
+            type: BottomNavigationBarType.fixed,
+          ),
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const AuthPage(),
+          'main': (context) => const MainLayout(),
+          'booking_page': (context) => BookingPage(),
+          'success_booking': (context) => const AppointmentBooked(),
+        },
+      ),
+    );
+  }
+}
